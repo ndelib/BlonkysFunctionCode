8592186048b8a3635723748d30b60ab653d54c7f
ndelib <69071207+ndelib@users.noreply.github.com>
2023-10-02 17:22:37 +1100

    Updated function code for stor2p0ezjly98h5qs7

--- ReadBlobFunction.cs
+++ ReadBlobFunction.cs
@@ -10,8 +10,8 @@ public static async Task<IActionResult> Run(
     [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req, 
     ILogger log)
 {
-    const string blobUri = "https://storups4y6hkxcqibfd.blob.core.windows.net/mycontainer/hello.txt";
-    const string sasToken = "?sv=2020-04-08&ss=b&srt=o&se=2023-10-02T07%3A07%3A21Z&sp=r&sig=mqFpjaa0WiKantzmVVHxw8iofPUh4KzuQPlvlwXBhXg%3D";
+    const string blobUri = "https://stor2p0ezjly98h5qs7.blob.core.windows.net/mycontainer/hello.txt";
+    const string sasToken = "?sv=2020-04-08&ss=b&srt=o&se=2023-10-02T07%3A22%3A37Z&sp=r&sig=zRqjNND7PNHJJ9xhnRLln31lNwIUM2zft0efSwaexeY%3D";
     var blobClient = new BlobClient(new Uri(blobUri + sasToken));
     var blobContent = await blobClient.OpenReadAsync();
     using var reader = new StreamReader(blobContent.Value.Content);
